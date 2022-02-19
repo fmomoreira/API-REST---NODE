38703454fb3377d6fe299f52f1efe1ac53d6bd98
fmomoreira <felipemoreira.frontend@gmail.com>
2022-02-19 07:42:55 -0300

    19022022 - Criando rota para criar pagamento

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -3,4 +3,10 @@ module.exports = function(app){
         console.log("Recebida requisiçaõ de teste.");
         res.send('Ok.');
     });
+
+    app.post('/pagamentos/pagamento', function(req, res){
+        let pagamento = req.body;
+        console.log(pagamento);
+        res.send('Voce esta na rota pagamentos/pagamento');
+    })
 }
